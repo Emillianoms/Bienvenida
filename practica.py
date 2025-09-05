e661ee68c0d88855edd3b7e7900487c2a2aecaf8
Emillianoms <emilianoms@isb.edu.mx>
2025-09-05 16:22:10 +0000

    PRACTICA

---
+++ practica.py
@@ -0,0 +1,24 @@
+=========================================================
+Cartel de Bienvenido - Ejemplo
+Este programa solo usa "Print" para mostrar un cartel en la pantalla. Tu puedes modificarlo y hacerlo en tu estilo. 🥔
+=========================================================
+
+print("****************************************************************")
+print("***************************¨¨¨**********************************")
+print("*****************¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨******************")
+print("*              BIENVENIDOS A SEXTO DE PRIMARIA                 *")
+print("*                   PROGRAMANDO EN PYTHON                      *")
+print("*                                                              *")
+print("*                                                              *")
+print("*              APRENDEREMOS A USAR LA COMPUTADORA              *")
+print("*               CRREAREMOS PROGRAMAS DIVERTIDOS                *")
+print("*              RESOLVEREMOS RETOS CON CREATIVIDAD              *")
+print("*                                                              *")
+print("*               *******************************                *")
+print("*                                                              *")
+print("*                      FRASE DEL DIA:                          *")
+print("*                   EL CONOCIMIENTO ES PODER.                  *")}
+print("*                                                              *")
+print("*               *******************************                *")
+print("*                                                              *")
+print("*                                                              *")
